a859ba0ddcf1a19d1b4d5ee828f5025a996e560e
phongtrann <phongtran99932@gmail.com>
2022-07-23 15:59:31 +0700

    Add Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
   late bool _obscureText = true;
   late bool _obscureTextConfirm = true;
 
-
   @override
   Widget build(BuildContext context) {
     const Color labelColor = Color(0xff1B447E);
@@ -25,173 +24,279 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
     return MaterialApp(
       title: 'Reset passwords',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          margin: const EdgeInsets.only(top: 40, left: 17, right: 17),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                  margin: const EdgeInsets.only(top: 15, left: 15, right: 15),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Icon(
+      // Navigator operation requested with a context that does not include a Navigator.
+        // add scaffold in builder
+      home: Builder (
+          builder: (BuildContext context) {
+            return Scaffold(
+              body: Container(
+                margin: const EdgeInsets.only(top: 40, left: 17, right: 17),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 66, left: 18),
+                        child: const Icon(
                           Icons.arrow_back,
                           color: labelColor,
                         ),
-                        Image.asset(
-                          'assets/logo_finbox.png',
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 60, right: 18),
+                        child: Image.asset(
+                          'assets/images/logo_finbox.png',
                           width: 99,
                           height: 26,
                         ),
-                      ])),
-              // TODO: lam tiep
-              const SizedBox(height: 35),
+                      ),
+                    ]),
+                    // TODO: lam tiep
+                    const SizedBox(height: 34),
 
-              const Text('Mật khẩu mới',
-                  style: TextStyle(
-                      color: labelColor,
-                      fontSize: 28,
-                      fontWeight: FontWeight.w700)),
-              const SizedBox(height: 7),
-              const Text('Bạn vui lòng nhập mật khẩu mới để hoàn tất.',
-                  style: TextStyle(
-                    color: labelColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  )),
-              const SizedBox(height: 8),
-              const SizedBox(
-                width: 42,
-                height: 3,
-                child: DecoratedBox(
-                  decoration: BoxDecoration(color: Color(0xff3563A3)),
+                    const Text('Mật khẩu mới',
+                        style: TextStyle(
+                            color: labelColor,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w700)),
+                    const SizedBox(height: 10),
+                    const Text('Bạn vui lòng nhập mật khẩu mới để hoàn tất.',
+                        style: TextStyle(
+                          color: labelColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        )),
+                    const SizedBox(height: 8),
+                    const SizedBox(
+                      width: 42,
+                      height: 3,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(color: Color(0xff3563A3)),
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+                    RichText(
+                        text: const TextSpan(
+                          text: 'Mật khẩu',
+                          style: TextStyle(
+                            color: Color(0xff777777),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                          ),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: '*',
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                ))
+                          ],
+                        )),
+                    const SizedBox(height: 14),
+                    TextField(
+                      obscureText: _obscureText,
+                      decoration: InputDecoration(
+                          suffixIcon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.only(right: 9.0),
+                                child: Text(
+                                  _obscureText ? 'Hiển thị' : 'Ẩn',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.only(right: 14.29),
+                                child: IconButton(
+                                  icon: Icon(
+                                    _obscureText
+                                        ? Icons.visibility_outlined
+                                        : Icons.visibility_off_outlined,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _obscureText = !_obscureText;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(6.0),
+                          ),
+                          hintText: 'Nhập mật khẩu của bạn',
+                          contentPadding: const EdgeInsets.only(
+                            left: 30.0,
+                            top: 18.0,
+                            bottom: 15.0,
+                          ),
+                          hintStyle: const TextStyle(color: Color(0xffCCCFD4))),
+                    ),
+                    const SizedBox(height: 16),
+                    RichText(
+                        text: const TextSpan(
+                          text: 'Xác nhận mật khẩu',
+                          style: TextStyle(
+                            color: Color(0xff777777),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                          ),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: '*',
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                ))
+                          ],
+                        )),
+                    const SizedBox(height: 14),
+                    TextField(
+                      obscureText: _obscureTextConfirm,
+                      decoration: InputDecoration(
+                          suffixIcon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.only(right: 9.0),
+                                child: Text(
+                                  _obscureTextConfirm ? 'Hiển thị' : 'Ẩn',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.only(right: 14.29),
+                                child: IconButton(
+                                  icon: Icon(
+                                    _obscureTextConfirm
+                                        ? Icons.visibility_outlined
+                                        : Icons.visibility_off_outlined,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _obscureTextConfirm = !_obscureTextConfirm;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(6.0),
+                          ),
+                          hintText: 'Xác nhận mật khẩu của bạn',
+                          contentPadding: const EdgeInsets.only(
+                            left: 30.0,
+                            top: 18.0,
+                            bottom: 15.0,
+                          ),
+                          hintStyle: const TextStyle(color: Color(0xffCCCFD4))),
+                    ),
+                    const SizedBox(height: 32),
+
+                    SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: buttonColor,
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => const SuccessfulConfirmationPage()),
+                            );
+                          },
+                          child: const Text(
+                            'XÁC NHẬN',
+                            style:
+                            TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                          ),
+                        )),
+                  ],
                 ),
               ),
-              const SizedBox(height: 35),
-              RichText(
-                  text: const TextSpan(
-                    text: 'Mật khẩu',
-                    style: TextStyle(
-                      color: Color(0xffCCCFD4),
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(text: '*',
-                          style: TextStyle(
-                            color: Colors.red,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                          ))
-                    ],
-                  )),
-              const SizedBox(height: 14),
-              TextField(
-                obscureText: _obscureText,
-                decoration: InputDecoration(
-                    suffixIcon: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.only(right: 9.0),
-                          child: Text(
-                            _obscureText ? 'Hiển thị' : 'Ẩn',
-                            style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 12,),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.only(right: 14.29),
-                          child: IconButton(
-                            icon: Icon(
-                              _obscureText ? Icons.visibility_outlined : Icons.visibility_off_outlined,
-                            ), onPressed: () {
-                            setState(() {
-                              _obscureText = !_obscureText;
-                            });
-                          },
-                          ),
-                        ),
-                      ],
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                    hintText: 'Nhập mật khẩu của bạn',
-                    hintStyle: const TextStyle(color: Color(0xffCCCFD4))),
-              ),
-              const SizedBox(height: 35),
-              RichText(
-                  text: const TextSpan(
-                    text: 'Xác nhận mật khẩu',
-                    style: TextStyle(
-                      color: Color(0xffCCCFD4),
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(text: '*',
-                          style: TextStyle(
-                            color: Colors.red,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
-                          ))
-                    ],
-                  )),
-              const SizedBox(height: 14),
-              TextField(
-                obscureText: _obscureTextConfirm,
-                decoration: InputDecoration(
-                    suffixIcon: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.only(right: 9.0),
-                          child: Text(
-                            _obscureTextConfirm ? 'Hiển thị' : 'Ẩn',
-                            style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 12,),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.only(right: 14.29),
-                           child: IconButton(
-                            icon: Icon(
-                              _obscureTextConfirm ? Icons.visibility_outlined : Icons.visibility_off_outlined,
-                            ), onPressed: () {
-                             setState(() {
-                               _obscureTextConfirm = !_obscureTextConfirm;
-                             });
-                           },
-                          ),
-                        ),
-                      ],
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                    hintText: 'Xác nhận mật khẩu của bạn',
-                    hintStyle: const TextStyle(color: Color(0xffCCCFD4))),
+            );
+          },
+      )
+    );
+  }
+}
 
-              ),
-              const SizedBox(height: 35),
+class SuccessfulConfirmationPage extends StatefulWidget {
+  const SuccessfulConfirmationPage({Key? key}) : super(key: key);
 
-              SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
+  @override
+  State<SuccessfulConfirmationPage> createState() =>
+      _SuccessfulConfirmationPageState();
+}
+
+class _SuccessfulConfirmationPageState
+    extends State<SuccessfulConfirmationPage> {
+  @override
+  Widget build(BuildContext context) {
+    const Color buttonColor = Color(0xff1E60C0);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                top: 158.0,
+                left: 120.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                        top: 50.0, right: 90.0),
+                    child: Image.asset(
+                      'assets/images/success.png',
+                      height: 135,
+                      width: 135,
+                    ),
+                  ),
+                  Container(
+                    child: const Text(
+                      'Đổi mật khẩu thành công',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       primary: buttonColor,
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                     child: const Text(
-                      'XÁC NHẬN',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                      'QUAY VỀ TRANG ĐĂNG NHẬP',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
                     ),
-                  )),
-            ],
-          ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
